cff01981edb61fe940ccfeab6d2ad993ed88acb2
MaximumCell <alihassanwinner@gmail.com>
2025-03-05 12:56:32 +0500

    testing 7

--- backend/src/index.js
+++ backend/src/index.js
@@ -70,10 +70,10 @@ cron.schedule("0 * * * *", () => {
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
