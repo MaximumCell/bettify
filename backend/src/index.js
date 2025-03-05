cf54c9e097a49ecd1e5e0eea31f731b2bbc92acb
MaximumCell <alihassanwinner@gmail.com>
2025-03-05 13:01:48 +0500

    testing 9

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ initializeSocket(httpServer);
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bettify.onrender.com"],
+    origin: ["http://localhost:3000", "https://bettify-frontend.onrender.com"],
     credentials: true,
   })
 );
